7e2f565e9433ea43f08a90a79b4105d8e0c90795
codeamor <ssm2319@gmail.com>
2021-02-22 18:41:27 +0900

    feat: CRUD 중 update 구현

--- practice_02/src/main/java/com/sumin/practice_02/Practice02Application.java
+++ practice_02/src/main/java/com/sumin/practice_02/Practice02Application.java
@@ -2,6 +2,7 @@ package com.sumin.practice_02;
 
 import com.sumin.practice_02.domain.Course;
 import com.sumin.practice_02.domain.CourseRepository;
+import com.sumin.practice_02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,19 +20,29 @@ public class Practice02Application {
     }
 
     // JPA 사용 이해를 위해 임시로 만든 코드
+
     @Bean
-    public CommandLineRunner demo(CourseRepository repository) {
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
         return (args) -> {
+            courseRepository.save(new Course("수민이의 Spring", "손수민"));
 
-            Course course1 = new Course("Spring", "손수민");
-            repository.save(course1);
-
-            List<Course> courseList = repository.findAll();
-
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
             for (int i = 0; i < courseList.size(); i++) {
-                Course c = courseList.get(i);
-                System.out.println(c.getTitle());
-                System.out.println(c.getTutor());
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+
+            Course new_course = new Course("수민이의 React", "손수민");
+            courseService.update(1L, new_course);
+            courseList = courseRepository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
             }
         };
     }
